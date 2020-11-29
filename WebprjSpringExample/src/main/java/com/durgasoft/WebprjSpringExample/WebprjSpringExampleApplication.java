6295c5ad5bc69e61889f2601202573a2f3f76b6e
esrinivasarao <67132085+esrinivasarao@users.noreply.github.com>
2020-11-29 13:50:25 +0000

    Web Project Example

---
+++ WebprjSpringExample/src/main/java/com/durgasoft/WebprjSpringExample/WebprjSpringExampleApplication.java
@@ -0,0 +1,14 @@
+package com.durgasoft.WebprjSpringExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprjSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprjSpringExampleApplication.class, args);
+		System.out.println("This is Spring Web Project Example ");
+	}
+
+}
